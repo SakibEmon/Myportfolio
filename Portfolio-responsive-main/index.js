9415744b1380d0065442744a9397bde8c9b65180
Sakib Emon <sakibemon001@gmail.com>
2025-08-24 12:31:08 +0600

    Update index.js

--- Portfolio-responsive-main/index.js
+++ Portfolio-responsive-main/index.js
@@ -7,16 +7,12 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   mobileMenu.classList.toggle("active");
 });
-
-// The classList property allows you to interact with the classes of an HTML element.
 menuItem.forEach((item) => {
   item.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     mobileMenu.classList.toggle("active");
   });
 });
-
-// after hero
 document.addEventListener("scroll", () => {
   var scroll_position = window.scrollY;
   if (scroll_position > 250) {
